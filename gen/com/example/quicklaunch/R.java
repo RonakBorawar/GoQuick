568a505ea0c061d7f3679866c31cb10e187f6680
Rahul Borawar <rahul.borawar@commonfloor.com>
2015-07-21 10:39:00 +0530

    Renamed some layout files

--- gen/com/example/quicklaunch/R.java
+++ gen/com/example/quicklaunch/R.java
@@ -21,38 +21,35 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int launchapp=0x7f020001;
-        public static final int orientation=0x7f020002;
-        public static final int proximity=0x7f020003;
-        public static final int shakeicon=0x7f020004;
-        public static final int soundicon=0x7f020005;
+        public static final int app_default_icon=0x7f020000;
+        public static final int app_icon=0x7f020001;
+        public static final int icon_proximity_gesture=0x7f020002;
+        public static final int icon_shake_x_gesture=0x7f020003;
+        public static final int icon_shake_y_gesture=0x7f020004;
+        public static final int icon_shake_z_gesture=0x7f020005;
+        public static final int splash_screen=0x7f020006;
     }
     public static final class id {
-        public static final int ImageView01=0x7f080007;
-        public static final int action_settings=0x7f080009;
-        public static final int app_icon=0x7f080003;
-        public static final int app_name=0x7f080004;
-        public static final int app_paackage=0x7f080005;
-        public static final int button1=0x7f080002;
-        public static final int listitem=0x7f080001;
-        public static final int textView1=0x7f080000;
-        public static final int textView2=0x7f080008;
-        public static final int thumb=0x7f080006;
+        public static final int ImageView01=0x7f070005;
+        public static final int app_icon=0x7f070001;
+        public static final int app_name=0x7f070002;
+        public static final int app_paackage=0x7f070003;
+        public static final int button1=0x7f070009;
+        public static final int imageView1=0x7f070000;
+        public static final int listitem=0x7f070008;
+        public static final int textView1=0x7f070006;
+        public static final int textView2=0x7f070007;
+        public static final int thumb=0x7f070004;
     }
     public static final class layout {
-        public static final int activity_first=0x7f030000;
-        public static final int activity_fourth=0x7f030001;
-        public static final int activity_second=0x7f030002;
-        public static final int activity_third=0x7f030003;
-    }
-    public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int activity_splash=0x7f030000;
+        public static final int app_list_item=0x7f030001;
+        public static final int app_list_view=0x7f030002;
+        public static final int gesture_list_item=0x7f030003;
+        public static final int gesture_list_view=0x7f030004;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
     }
     public static final class style {
         /** 
